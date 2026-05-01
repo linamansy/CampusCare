37c9bcc98f880ba77d8925ab0150649c5f2add06
linamansy <linamansy9@gmail.com>
2026-05-01 23:21:51 +0300

    Initial backend setup (users + issues API)

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers/userController');
+
+router.get('/', controller.getAllUsers);
+router.post('/', controller.createUser);
+
+module.exports = router;
